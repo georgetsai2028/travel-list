a279edf51a2d86e5a299509740957dc1e4ae0790
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-04 15:35:45 -0700

    created basic static functions

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,29 @@ function App() {
   
   return (
     <>
-      Hello
+    <Logo />
     </>
   )
 }
 
 function Form(){
-  
+  return (
+    <div className = "add-form">
+      <h3>What do you need to bring? 🧳 </h3>
+    </div>
+  )
 }
 
 function Logo(){
-
+  return <h1>🏝️Travel List🤑</h1>
 }
 
 function PackingList(){
-
+  return <div className='list'> List </div>
 }
 
 function Stats(){
-  
+  return <footer> You have X items on your list and already packed X %</footer>
 }
 
 export default App
