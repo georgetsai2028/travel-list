3295f6373d0b2d494539a530284b559abea39c28
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-08 18:25:14 -0700

    added initial items array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false},
+  { id: 2, description: "Socks", quantity: 12, packed: false},
+];
+
 function App() {
+
   
   return (
     <>
