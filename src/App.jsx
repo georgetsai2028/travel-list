fc1fe8e76f78cf545f374329cb9e4ee2bbad42ac
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-08 19:21:50 -0700

    added item function

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false},
-  { id: 2, description: "Socks", quantity: 12, packed: false},
+  { id: 1, description: "Passports", quantity: 2, packed: true},
+  { id: 2, description: "Socks", quantity: 8, packed: false},
+  { id: 3, description: "Charger", quantity: 1, packed: true},
 ];
 
 function App() {
@@ -41,10 +42,18 @@ function PackingList(){
   </ul>
 </div>
   );}
+
 function Item({item}){
-  return <li>{item.description}</li>
+  return(
+  <li>
+    <span style={item.packed ? {textDecoration: 'line-through'} : {}}> {item.quantity} {item.description}</span>
+    <button>❌</button>
+  </li>
+
+  )
 
 }
+
 function Stats(){
   return <footer className='stats'> <em>You have X items on your list and already packed X % </em> </footer>
 }
