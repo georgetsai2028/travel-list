fadacb62b61b488e561cc0a4bb77b07b2bc6b3d1
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-11 14:52:35 -0700

    testing api

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import FlashCard from './FlashCard.jsx'
 import App from './App.jsx'
+import PlayerStats from './PlayerStats.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FlashCard />
+    <PlayerStats />
   </StrictMode>,
 )
