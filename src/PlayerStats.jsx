22ca19cc9704c5d04b98b4b3563bddd6a779d8e3
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-11 11:06:41 -0700

    testing api

---
+++ src/PlayerStats.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function PlayerStats(){
+    const [username, setUsername] = useState("");
+    const [playerStats, setPlayerStats] = useState(null);
+    
+    const API_KEY = 
+}
